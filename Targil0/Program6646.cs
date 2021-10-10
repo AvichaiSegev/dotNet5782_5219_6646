1317b976d2c2403137719c34d8166c10177c9802
beya421 <bhalevy@g.jct.ac.il>
2021-10-10 17:51:13 +0300

    commit alpah

---
+++ Targil0/Program6646.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program6646
+    {
+        static void Main()
+        {
+            Console.WriteLine("HooHoo!");
+        }
+    }
+}
