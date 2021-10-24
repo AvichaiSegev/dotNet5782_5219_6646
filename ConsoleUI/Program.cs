807b0c040f2bc0c18b19b06b9e4f9cfd6fb29ab8
beya421 <bhalevy@g.jct.ac.il>
2021-10-24 19:47:03 +0300

    Menu program: split to functions.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,59 +4,56 @@ namespace ConsoleUI
 {
     class Program
     {
-        static int MainMenu()
+        static int options(string text)
         {
-            int firstChoice;
-            Console.WriteLine("Welcome to our program!\n=====Choose one of the follow options:=====");
-            Console.WriteLine("1. Insert options.");
-            Console.WriteLine("2. Update options.");
-            Console.WriteLine("3. Display options.");
-            Console.WriteLine("4. List view options.");
-            Console.WriteLine("5. Exit.");
-            int.TryParse(Console.ReadLine(), out firstChoice);
-            return firstChoice;
+            int Choice;
+            Console.WriteLine(text);
+            int.TryParse(Console.ReadLine(), out Choice);
+            return Choice;
         }
         static void Main(string[] args)
         {
             int firstChoice = 0, secondChoice = 0;
             while (firstChoice != 5)
             {
-                firstChoice = MainMenu();
+                firstChoice = options("Welcome to our program!\n=====Choose one of the follow options:=====\n" +
+                    "1. Insert options.\n" +
+                    "2. Update options.\n" +
+                    "3. Display options.\n" +
+                    "4. List view options.\n" +
+                    "5. Exit.");
                 switch (firstChoice)
                 {
                     case 1:
-                        Console.WriteLine("=====Insert options=====");
-                        Console.WriteLine("1. Add a base station to the list of stations.");
-                        Console.WriteLine("2. Add a drone to the list of existing drones.");
-                        Console.WriteLine("3. Addition of a new customer to the customer list.");
-                        Console.WriteLine("4. Receipt of package for shipment.");
-                        int.TryParse(Console.ReadLine(), out secondChoice);
+                        secondChoice = options("=====Insert options=====\n" +
+                            "1. Add a base station to the list of stations.\n" +
+                            "2. Add a drone to the list of existing drones.\n" +
+                            "3. Addition of a new customer to the customer list.\n" +
+                            "4. Receipt of package for shipment.");
                         break;
                     case 2:
-                        Console.WriteLine("=====Update options=====");
-                        Console.WriteLine("1. Assign a package to a drone.");
-                        Console.WriteLine("2. Collection of a package by drone.");
-                        Console.WriteLine("3. Delivery package to customer.");
-                        Console.WriteLine("4. Sending a drone for charging at a base station.");
-                        Console.WriteLine("5. Release drone from charging at base station.");
-                        int.TryParse(Console.ReadLine(), out secondChoice);
+                        secondChoice = options("=====Update options=====\n" +
+                            "1. Assign a package to a drone.\n" +
+                            "2. Collection of a package by drone.\n" +
+                            "3. Delivery package to customer.\n" +
+                            "4. Sending a drone for charging at a base station.\n" +
+                            "5. Release drone from charging at base station.");
                         break;
                     case 3:
-                        Console.WriteLine("=====Display options=====");
-                        Console.WriteLine("1. Base station view.");
-                        Console.WriteLine("2. Drone view.");
-                        Console.WriteLine("3. Customer view.");
-                        Console.WriteLine("4. Package view.");
-                        int.TryParse(Console.ReadLine(), out secondChoice);
+                        secondChoice = options("=====Display options=====\n" +
+                            "1. Base station view.\n" +
+                            "2. Drone view.\n" +
+                            "3. Customer view.\n" +
+                            "4. Package view.");
                         break;
                     case 4:
-                        Console.WriteLine("=====List view options=====");
-                        Console.WriteLine("1. Displays a list of base stations.");
-                        Console.WriteLine("2. Displays the list of drones.");
-                        Console.WriteLine("3. View customer list.");
-                        Console.WriteLine("4. Displays the list of packages.");
-                        Console.WriteLine("5. Displays a list of packages not yet associated with a drone.");
-                        Console.WriteLine("6. Display base stations with available charging stations.");    
+                        secondChoice = options("=====List view options=====\n" +
+                        "1. Displays a list of base stations.\n" +
+                        "2. Displays the list of drones.\n" +
+                        "3. View customer list.\n" +
+                        "4. Displays the list of packages.\n" +
+                        "5. Displays a list of packages not yet associated with a drone.\n" +
+                        "6. Display base stations with available charging stations.");    
                         break;
                     default:
                         break;
